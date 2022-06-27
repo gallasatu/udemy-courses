77eb91d9861dbd05928178df26fa6283c4c1869b
id_rsa_gallasatu <kujjawskia.gmail>
2022-06-27 11:13:51 +0200

    hamburger menu

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-const input = document.querySelector('input')
-const btnConvert = document.querySelector('.conv')
-const btnReset = document.querySelector('.reset')
-const btnChange = document.querySelector('.change')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
-const result = document.querySelector('.result')
-let temp
-let fahrenheit
-let celsius
+const burgerBtn = document.querySelector('.burger')
+const barsIco = document.querySelector('.fa-bars')
+const xIco = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
 
-
-const getTemp = () => {
-  temp = input.value
+const handleNav = () => {
+  nav.classList.toggle('active')
+  burgerBtn.classList.toggle('active')
+  barsIco.classList.toggle('hide')
+  xIco.classList.toggle('hide')
 }
 
-const convert = () => {
-  if (one.textContent == '°C') {
-  fahrenheit = (temp * 9/5) + 32
-  result.textContent = `${temp}°C = ${fahrenheit}°F`
-  } else {
-    celsius = (temp - 32) * 5/9
-    result.textContent = `${temp}°F = ${celsius}°C`
-  }
-}
-
-const reset = () => {
-  input.value = ''
-  result.textContent = ''
-}
-
-const change = () => {
-  console.log(one, two)
-  if (one.textContent == '°C') {
-    one.textContent = '°F'
-    two.textContent = '°C'
-  } else {
-    one.textContent = '°C'
-    two.textContent = '°F'
-  }
-  result.textContent = ''
-}
-
-input.addEventListener('input', getTemp)
-btnConvert.addEventListener('click', convert)
-btnReset.addEventListener('click', reset)
-btnChange.addEventListener('click', change)
+burgerBtn.addEventListener('click', handleNav)
