ef81658dd3aacef031b49621dd65465780ff1024
id_rsa_gallasatu <kujjawskia.gmail>
2022-07-01 09:55:21 +0200

    dog api

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const btn1 = document.querySelector('.btn-1')
-const btn2 = document.querySelector('.btn-2')
-const btn3 = document.querySelector('.btn-3')
-const p = document.querySelector('p')
+const button = document.querySelector('button')
+const img = document.querySelector('img')
 
-function Menu(name, price) {
-    this.name = name
-    this.price = price
-  }
+const URL ='https://dog.ceo/api/breeds/image/random'
 
-  const newMenu1 = new Menu('Schabowy', 29)
-  const newMenu2 = new Menu('Pizza', 25)
-  const newMenu3 = new Menu('Zupa', 8)
-
-Menu.prototype.showMenu = function () {
-  console.log(`${this.name} kosztuje ${this.price} zł.`)
-}
-
-btn1.addEventListener('click', () => newMenu1.showMenu())
-btn2.addEventListener('click', () => newMenu2.showMenu())
-btn3.addEventListener('click', () => newMenu3.showMenu())
-
+button.addEventListener('click', () => {
+fetch(URL)
+.then(res => res.json())
+.then(data => img.setAttribute('src', data.message))
+.catch(err => console.error(err))
+})
